cb2aa4ed8ab5ab5d851f108696134093b28257b8
Varun Upadhyay <varun.802.vu@gmail.com>
2025-01-04 13:29:35 +0530

    added feed to the redux

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
 
 const Feed = () => {
-
+  const dispatch = useDispatch();
   const updateFeed = async() =>{
     const res = await axios.get(BASE_URL +"/user/feed",{withCredentials:true}) ;
-    console.log(res.data)
+    //we have to store that in redux (just like addUser we have to make one more slice for this seperately)
+    dispatch(addFeed(res.data));
   }
 
   useEffect(()=>{
